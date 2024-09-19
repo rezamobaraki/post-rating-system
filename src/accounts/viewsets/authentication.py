810d50a431aea8445551a5ab19d5a20a556e1a53
Reza Mobaraki <rezam578@gmail.com>
2024-09-19 11:39:19 +0330

    Fix(authentication): fix serializer names on viewsets

--- src/accounts/viewsets/authentication.py
+++ src/accounts/viewsets/authentication.py
@@ -1,17 +1,10 @@
 from rest_framework import status
 
 from accounts.serialzers import RegisterSerializer
+from accounts.serialzers.authentication import LoginSerializer
 from commons.mixins import FixStatusMixin
 from commons.throttles import LoginRateThrottle, RegisterRateThrottle
 from commons.viewsets import CreateModelViewSet
-
-
-class LoginViewSet(FixStatusMixin, CreateModelViewSet):
-    fix_status = status.HTTP_200_OK
-    authentication_classes = []
-    permission_classes = []
-    throttle_classes = [LoginRateThrottle]
-    serializer_class = RegisterSerializer
 
 
 class RegisterViewsSet(FixStatusMixin, CreateModelViewSet):
@@ -20,3 +13,11 @@ class RegisterViewsSet(FixStatusMixin, CreateModelViewSet):
     permission_classes = []
     throttle_classes = [RegisterRateThrottle]
     serializer_class = RegisterSerializer
+
+
+class LoginViewSet(FixStatusMixin, CreateModelViewSet):
+    fix_status = status.HTTP_200_OK
+    authentication_classes = []
+    permission_classes = []
+    throttle_classes = [LoginRateThrottle]
+    serializer_class = LoginSerializer
