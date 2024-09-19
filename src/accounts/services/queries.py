e1ddabb4e825c0be7814ce7847cb534794c41640
Reza Mobaraki <rezam578@gmail.com>
2024-09-19 11:56:54 +0330

    fix comments

--- src/accounts/services/queries.py
+++ src/accounts/services/queries.py
@@ -5,6 +5,4 @@ User = get_user_model()
 # In the case that we need custom field to authenticate user, we can use this function
 def authenticate_user(*, username: str, password: str) -> User:
     user = User.objects.filter(username=username).first()
-    if user and user.check_password(password):
-        return user
-    return None
+    return user if user and user.check_password(password) else None
