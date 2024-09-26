110097ed92a0ce9813674a8cd141baeec13d2dcd
Reza Mobaraki <rezam578@gmail.com>
2024-09-26 19:48:34 +0330

    add pagination class

--- src/posts/viewsets/post.py
+++ src/posts/viewsets/post.py
@@ -1,3 +1,4 @@
+from commons.pagination import StandardResultsSetPagination
 from commons.viewsets import ListModelViewSet
 from posts.models import Post
 from posts.serialzers.post import PostSerializer
@@ -6,6 +7,7 @@ from posts.serialzers.post import PostSerializer
 class PostViewSet(ListModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    pagination_class = StandardResultsSetPagination
 
     def get_queryset(self):
         return self.queryset.prefetch_related('stat')
